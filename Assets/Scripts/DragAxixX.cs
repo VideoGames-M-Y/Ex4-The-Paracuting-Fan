8abdcb0659dfd986c4d120ff0088084cff16a7e2
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-11 09:50:36 +0200

    Update DragAxixX.cs

--- Assets/Scripts/DragAxixX.cs
+++ Assets/Scripts/DragAxixX.cs
@@ -5,41 +5,45 @@ using UnityEngine;
  * and drop it by releasing the mouse.
  */
 [RequireComponent(typeof(Rigidbody))]
-public class DragAxixX : MonoBehaviour {
-
+public class DragAxixX : MonoBehaviour
+{
     [Header("These fields are for display only")]
     [SerializeField] private Vector3 positionMinusMouse;
     [SerializeField] private float screenZCoordinate;
-    
+
     private Rigidbody rb;
-    private void Start() {
+
+    private void Start()
+    {
         rb = GetComponent<Rigidbody>();
     }
-    
+
     // This function is called when the player clicks the collider of this object.
-    void OnMouseDown() {
-        if (!rb.IsSleeping()) return;  // Do not allow the player to drag the object when it is moving.
-        rb.isKinematic = true; // ignore forces, since the player now moves the object.
+    private void OnMouseDown()
+    {
+        if (!rb.IsSleeping()) return; // Do not allow the player to drag the object when it is moving.
+        rb.isKinematic = true; // Ignore forces, since the player now moves the object.
         screenZCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         positionMinusMouse = transform.position - MousePositionOnWorld();
     }
 
     // This function is called when the player drags the mouse.
-    void OnMouseDrag() {
-        if (!rb.IsSleeping()) return;  // Do not allow the player to drag the object when it is moving.
+    private void OnMouseDrag()
+    {
+        if (!rb.IsSleeping()) return; // Do not allow the player to drag the object when it is moving.
         transform.position = positionMinusMouse + MousePositionOnWorld();
     }
 
     // This function is called when the player releases the mouse button.
-    void OnMouseUp() {
+    private void OnMouseUp()
+    {
         rb.isKinematic = false;
     }
-        
-    private Vector3 MousePositionOnWorld() {
-        Vector3 mouseOnScreen = Input.mousePosition;    // Screen coordinates of mouse (x,y)
-        mouseOnScreen.z = screenZCoordinate;            // z coordinate of game object on screen
-        Vector3 mouseOnWorld = Camera.main.ScreenToWorldPoint(mouseOnScreen);
-        return mouseOnWorld;
+
+    private Vector3 MousePositionOnWorld()
+    {
+        Vector3 mouseOnScreen = Input.mousePosition; // Screen coordinates of mouse (x,y)
+        mouseOnScreen.z = screenZCoordinate;         // z coordinate of game object on screen
+        return Camera.main.ScreenToWorldPoint(mouseOnScreen);
     }
 }
-
